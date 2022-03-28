2060ffe3164d09bd67ad87e9e31684141d81544c
Hypervisor <hyp3rvisor@protonmail.com>
2022-03-27 20:26:59 -0700

    optimize node retrieval

--- src/NtfsReader/System/IO/Filesystem/Ntfs/NtfsReader.Public.cs
+++ src/NtfsReader/System/IO/Filesystem/Ntfs/NtfsReader.Public.cs
@@ -111,33 +111,52 @@ namespace System.IO.Filesystem.Ntfs
 
             List<INode> nodes = new List<INode>();
 
-            //TODO use Parallel.Net to process this when it becomes available
-            UInt32 nodeCount = (UInt32)_nodes.Length;
-            for (UInt32 i = 0; i < nodeCount; ++i)
-                if (_nodes[i].NameIndex != 0 && GetNodeFullNameCore(i).StartsWith(rootPath, StringComparison.InvariantCultureIgnoreCase))
-                    nodes.Add(new NodeWrapper(this, i, _nodes[i]));
+            /*
+                If the rootPath is equal to the root directory of the drive,
+                we can avoid running string comparisons on every node.
+
+                TODO: use Parallel.Net to process this when it becomes available
+            */
+            if (String.Compare(rootPath, _driveInfo.Name) == 0)
+            {
+                UInt32 nodeCount = (UInt32)_nodes.Length;
+                for (UInt32 i = 0; i < nodeCount; ++i)
+                    if (_nodes[i].NameIndex != 0)
+                        nodes.Add(new NodeWrapper(this, i, _nodes[i]));
+            }
+            else
+            {
+                UInt32 nodeCount = (UInt32)_nodes.Length;
+                for (UInt32 i = 0; i < nodeCount; ++i)
+                    if (_nodes[i].NameIndex != 0 && GetNodeFullNameCore(i).StartsWith(rootPath, StringComparison.InvariantCultureIgnoreCase))
+                        nodes.Add(new NodeWrapper(this, i, _nodes[i]));
+            }
 
             stopwatch.Stop();
-
-            Trace.WriteLine(
-                string.Format(
-                    "{0} node{1} have been retrieved in {2} ms",
-                    nodes.Count,
-                    nodes.Count > 1 ? "s" : string.Empty,
-                    (float)stopwatch.ElapsedTicks / TimeSpan.TicksPerMillisecond
-                )
-            );
 
             return nodes;
         }
 
         public IEnumerable<INode> EnumerateNodes(string rootPath)
         {
-            //TODO use Parallel.Net to process this when it becomes available
-            UInt32 nodeCount = (UInt32)_nodes.Length;
-            for (UInt32 i = 0; i < nodeCount; ++i)
-                if (_nodes[i].NameIndex != 0 && GetNodeFullNameCore(i).StartsWith(rootPath, StringComparison.InvariantCultureIgnoreCase))
-                    yield return new NodeWrapper(this, i, _nodes[i]);
+            /*
+                If the rootPath is equal to the root directory of the drive,
+                we can avoid running string comparisons on every node.
+            */
+            if (String.Compare(rootPath, _driveInfo.Name) == 0)
+            {
+                UInt32 nodeCount = (UInt32)_nodes.Length;
+                for (UInt32 i = 0; i < nodeCount; ++i)
+                    if (_nodes[i].NameIndex != 0)
+                        yield return new NodeWrapper(this, i, _nodes[i]);
+            }
+            else
+            {
+                UInt32 nodeCount = (UInt32)_nodes.Length;
+                for (UInt32 i = 0; i < nodeCount; ++i)
+                    if (_nodes[i].NameIndex != 0 && GetNodeFullNameCore(i).StartsWith(rootPath, StringComparison.InvariantCultureIgnoreCase))
+                        yield return new NodeWrapper(this, i, _nodes[i]);
+            }
         }
 
         public byte[] GetVolumeBitmap()
